a438cbfd3af1cff84172bf41ae408b508e17eb78
Alfred Gauthier <alfgoto@gmail.com>
2026-08-08 12:52:06 +0200

    fix: shared cookies

--- packages/i18n/src/routing.ts
+++ packages/i18n/src/routing.ts
@@ -4,6 +4,10 @@ export const routing = defineRouting({
   locales: ["en", "fr"],
   defaultLocale: "en",
   localePrefix: "as-needed",
+  localeCookie: {
+    name: "NEXT_LOCALE",
+    domain: process.env.NODE_ENV === "production" ? ".basalf.com" : undefined,
+  },
 });
 
 export type Locale = (typeof routing.locales)[number];
